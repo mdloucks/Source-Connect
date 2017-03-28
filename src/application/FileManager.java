05967e2a4c77a7558bed024fd982d8163715acf8
loucks12345 <loucks12345@gmail.com>
2017-03-27 22:53:06 -0400

    added file transfer stuff

--- src/application/FileManager.java
+++ src/application/FileManager.java
@@ -1,9 +1,15 @@
 package application;
 
 import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
 
 public class FileManager {
@@ -115,6 +121,55 @@ public class FileManager {
 			// add that file to the stage
 			stage.add(file);
 		}
+	
+	
+	}
+	
+	public void FileClient(String host, int port, String file) {
+		
+		Socket s;
+		
+		try {
+			s = new Socket(host, port);
+			sendFile(file, s);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}		
+	}
+	
+	public void sendFile(String file, Socket s) throws IOException {
+		DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+		FileInputStream fis = new FileInputStream(file);
+		byte[] buffer = new byte[4096];
+		
+		while (fis.read(buffer) > 0) {
+			dos.write(buffer);
+		}
+		
+		fis.close();
+		dos.close();	
+	}
+	
+	public void saveFile(Socket clientSock) throws IOException {
+		// for receiving data
+		DataInputStream dis = new DataInputStream(clientSock.getInputStream());
+		// for writing data to a file
+		FileOutputStream fos = new FileOutputStream("testfile.jpg");
+		byte[] buffer = new byte[4096];
+		
+		int filesize = 15123; // Send file size in separate msg
+		int read = 0;
+		int totalRead = 0;
+		int remaining = filesize;
+		while((read = dis.read(buffer, 0, Math.min(buffer.length, remaining))) > 0) {
+			totalRead += read;
+			remaining -= read;
+			System.out.println("read " + totalRead + " bytes.");
+			fos.write(buffer, 0, read);
+		}
+		
+		fos.close();
+		dis.close();
 	}
 	
 	private static class Commit {
@@ -122,15 +177,19 @@ public class FileManager {
 		private ArrayList<File> elements = new ArrayList<File>();
 		private String msg;
 		
+		@SuppressWarnings("unused")
 		public ArrayList<File> getElements() {
 			return elements;
 		}
+		@SuppressWarnings("unused")
 		public void setElements(ArrayList<File> elements) {
 			this.elements = elements;
 		}
+		@SuppressWarnings("unused")
 		public String getMsg() {
 			return msg;
 		}
+		@SuppressWarnings("unused")
 		public void setMsg(String msg) {
 			this.msg = msg;
 		}
--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,9 @@
 package application;
 	
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -7,13 +11,28 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 
-public class Main extends Application {
+public class Main extends Application implements Runnable {
 
+	private static FileManager fm;
+	private static ServerSocket ss;
+	
+	
 	@Override
 	public void start(Stage window) {
 		
 		try {
+
 			Parent root = FXMLLoader.load(getClass().getResource("/application/Main.fxml"));
+			
+			fm = new FileManager();
+			ss = new ServerSocket();
+			
+			// clients network info
+			//fm.FileClient(host, port, file);
+			
+			Thread t1 = new Thread(new Main());
+			t1.start();
+			
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			window.setScene(scene);
@@ -26,5 +45,20 @@ public class Main extends Application {
 
 	public static void main(String[] args) {
 		launch(args);
+	}
+
+	/**
+	 * checks for new incoming files
+	 */
+	public void run() {
+		while (true) {
+			try {
+				ss = new ServerSocket(5555);
+				Socket clientSock = ss.accept();
+				fm.saveFile(clientSock);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
