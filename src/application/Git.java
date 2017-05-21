e35a2dce81dd170e4c9f1ab8517aa476de4ae18e
Rane Lafraze <lafrazerl@gmail.com>
2017-05-20 20:42:45 -0400

    Simplify execute method and add multiple OS support

--- src/application/Git.java
+++ src/application/Git.java
@@ -6,20 +6,22 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
+import application.Main.OperatingSystem;
+
 /**
- * the actual end class which takes the data from this machine 
- * and transfers/sorts it with git and github
+ * the actual end class which takes the data from this machine and
+ * transfers/sorts it with git and github
  * 
  * 
  * @author louck
  *
  */
 public class Git {
-	
+
 	private static ArrayList<File> repositories = new ArrayList<File>();
 
 	private static ArrayList<String> ignores = new ArrayList<String>();
-	
+
 	/**
 	 * executes the given command in the users command shell
 	 * 
@@ -27,56 +29,49 @@ public class Git {
 	 */
 	public void execute(String command) {
 
-		String executable = "";
-		
-		switch(Main.OS) {
-		
-			case Windows:
-				executable = "cmd.exe";
-				break;
-				
-			case MacOS:
-				executable = "/Applications/Utilities/Terminal.app";
-				break;
-				
-			case Linux:
-				break;
-				
-			case Other:
-				System.out.println("cannot execute the given command because this machine's operating system is unspecified");
-				break;
-		
-			default:
-				break;
+		System.out.println("executing command \"" + command);
+
+		switch (Main.OS) {
+
+		case Windows:
+
+			try {
+				// /C Carries out the command specified by string and then
+				// terminates
+				ProcessBuilder builder = new ProcessBuilder("cmd.exe", "/c", command);
+				builder.redirectErrorStream(true);
+				Process p = builder.start();
+				BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
+				String line;
+				while (true) {
+					line = r.readLine();
+					if (line == null) {
+						break;
+					}
+					System.out.println(line);
+				}
+
+			} catch (IOException e) {
+
+			}
+
+			break;
+
+		case Other:
+			System.out.println("cannot execute the given command because this machine's operating system is unspecified");
+			break;
+
+		default:
+			try {
+				Runtime.getRuntime().exec(command);
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+			break;
 		}
-		
-		System.out.println("executing command \"" + command +  "\" with " + executable);
-		
-		
-		try {
-			Runtime.getRuntime().exec(command);
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
-		
-		try {
-			// /C Carries out the command specified by string and then terminates
-	        ProcessBuilder builder = new ProcessBuilder(executable, "/c", command);
-	            builder.redirectErrorStream(true);
-	            Process p = builder.start();
-	            BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
-	            String line;
-	            while (true) {
-	                line = r.readLine();
-	                if (line == null) { break; }
-	                System.out.println(line);
-	            }
-		
-		} catch(IOException e) {
-			
-		}
+
 	}
-	
+
 	public static ArrayList<File> getRepositories() {
 		return repositories;
 	}
