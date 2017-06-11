f2f31707c0209c12f7da99787fb87922b27abbfb
Rane Lafraze <lafrazerl@gmail.com>
2017-06-10 22:06:04 -0400

    Merge: config-path <--> develop
    Change constructor and add path variable

--- src/application/Repository.java
+++ src/application/Repository.java
@@ -2,8 +2,6 @@ package application;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 
 import javafx.stage.DirectoryChooser;
